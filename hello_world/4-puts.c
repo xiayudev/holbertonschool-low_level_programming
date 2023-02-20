843b81ca8d7336e3410eeb262daaa34b7ccf94d2
xiayudev <jctuesta7@gmail.com>
2023-02-19 18:47:09 -0800

    fix: fix file 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
-int main (void)
+/**
+ * main - Entry point
+ *
+ * Print a phrase using puts() function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
 {
 	char phrase[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(phrase);
+
 	return (0);
 }
