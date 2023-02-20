5ad26783f5af461d4359ddacafbe55de2979b021
xiayudev <jctuesta7@gmail.com>
2023-02-19 18:55:42 -0800

    feat: add file 5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Print a phrase using printf() function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char phrase[] = "with proper grammar, but the outcome is a piece of art,\n";
+
+	printf("%s", phrase);
+
+	return (0);
+}
