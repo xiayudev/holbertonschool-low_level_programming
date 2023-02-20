06e4fabce66ebbd242a66b9200d17de938496145
xiayudev <jctuesta7@gmail.com>
2023-02-19 21:08:30 -0800

    feat: add file 101-quote.c

---
+++ hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Print a phrase in the stderr
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char phrase[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fwrite(phrase, 1, sizeof(phrase), stderr);
+
+	return (1);
+}
