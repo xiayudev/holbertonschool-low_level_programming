49de0c71432a8d6388df550779574733b63403c2
xiayudev <jctuesta7@gmail.com>
2023-02-20 21:11:05 -0800

    feat: add file 9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Print all possible combinations of single-digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int num;
+
+	for (i = 0; i < 10; i++)
+	{
+		num = i + '0'; 
+		putchar(num);
+		if (num == '9')
+			break;
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
