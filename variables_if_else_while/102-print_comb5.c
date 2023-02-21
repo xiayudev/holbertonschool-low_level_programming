d21b4d442974801013e9d2e5f25356e95b512fba
xiayudev <jctuesta7@gmail.com>
2023-02-21 15:06:47 -0800

    fix: fix betty style

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -11,17 +11,18 @@
  */
 int main(void)
 {
-	int i,j;
+	int i;
+	int j;
 
 	for (i = 0; i < 100; i++)
 	{
 		for (j = i + 1; j < 100; j++)
 		{
-			putchar((i/10) + '0');
-			putchar((i%10) + '0');
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
 			putchar(' ');
-			putchar((j/10) + '0');
-			putchar((j%10) + '0');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
 			if (i != 98)
 			{
 				putchar(',');
