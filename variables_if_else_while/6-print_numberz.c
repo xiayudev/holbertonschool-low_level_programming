2f03d2c23e11d7e699b4af5e02f6937584e8032f
xiayudev <jctuesta7@gmail.com>
2023-02-20 20:27:14 -0800

    feat: add file 6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Print all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+
+	putchar('\n');
+
+	return (0);
+}
