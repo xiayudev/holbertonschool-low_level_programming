7b36aede13ed286a168d2e4b594bb821a7691fba
xiayudev <jctuesta7@gmail.com>
2023-02-20 19:27:22 -0800

    feat: add file 2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Print the alphabet in lowercase, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	size_t i;
+
+	for (i = 0; i < sizeof(alphabet) - 1; i++)
+		putchar(alphabet[i]);
+
+	putchar('\n');
+
+	return (0);
+}
