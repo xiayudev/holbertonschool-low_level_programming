c32c0aeeb2871f181958bb731da8dbe8febbe202
xiayudev <jctuesta7@gmail.com>
2023-02-22 10:00:52 -0800

    feat: add file 10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - function
+ * @a: First number
+ * @b: Second number
+ * Adds two numbers
+ *
+ * Return: The sum of two numbers
+ *
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
