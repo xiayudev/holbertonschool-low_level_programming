7b76c1612c20916db253084f057daed3fdaa79ed
xiayudev <jctuesta7@gmail.com>
2023-02-21 20:22:50 -0800

    fix: fix file 1-alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -2,22 +2,6 @@
 
 
 /**
- * main - Entry point
- *
- * Prints all lowercase alphabet
- *
- * Return: Always 0 (Success)
- */
-
-int main(void)
-{
-	print_alphabet();
-	_putchar('\n');
-	return (0);
-}
-
-
-/**
  * print_alphabet - function
  *
  * Prints all lowercase alphabet
@@ -31,4 +15,5 @@ void print_alphabet(void)
 
 	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
+	_putchar('\n');
 }
