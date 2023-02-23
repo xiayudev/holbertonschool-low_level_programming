8090ac9cdbbd98a7d76fa26b3b0a3e6fa931edce
xiayudev <jctuesta7@gmail.com>
2023-02-22 18:56:19 -0800

    feat: add file 100-times_table.c

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_times_table - function
+ * @n: The number to be printed
+ * Print the n times table
+ *
+ * Return: void
+ *
+ */
+void print_times_table(int n)
+{
+	int i = 0, j, increment;
+
+	if (n > 0 && n < 16)
+	{
+		while (i <= n)
+		{
+			j = 0;
+			increment = 0;
+			while (j <= n)
+			{
+				if (j != n)
+				{
+					if (increment + i > 9 && increment + i < 100)
+						printf("%d,  ", increment);
+					else if (increment + i > 99)
+						printf("%d, ", increment);
+					else 
+						printf("%d,   ", increment);
+				}
+				else
+					printf("%d", increment);
+
+				increment += i;
+				j++;
+			}
+			printf("\n");
+			i++;
+		}
+	}
+}
