97e7d57a2daab54e6757ee888965bcb773d22428
xiayudev <jctuesta7@gmail.com>
2023-02-22 19:40:02 -0800

    fix: fix condition

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -14,17 +14,18 @@ int  main(void)
 	int i, num = 1024;
 	int mult_5 = 0;
 	int mult_3 = 0;
+	int mult_35 = 0;
 
 	for (i = 1; i < num; i++)
 	{
 		if ((i % 5 == 0) && (i % 3 == 0))
-			continue;
+			mult_35 += i;
 		else if (i % 5 == 0)
 			mult_5 += i;
 		else if (i % 3 == 0)
 			mult_3 += i;
 	}
 
-	printf("%d\n", mult_5 + mult_3);
+	printf("%d\n", mult_5 + mult_3 + mult_35);
 	return (0);
 }
