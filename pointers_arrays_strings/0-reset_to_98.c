dae08ac52e2bdc30521b24b6a17a49d033dd9c41
xiayudev <jctuesta7@gmail.com>
2023-02-27 20:14:45 -0800

    feat: add README and file 0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+ * reset_to_98 - function
+ * @n: The character to be evaluated
+ * Updates the value of n
+ *
+ * Return: Void
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+# Project about pointers, arrays and strings
+In this chapter we will cover:
+- Pointers, pointer to pointers, pointer to arrays.
+- Strings and arrays.
