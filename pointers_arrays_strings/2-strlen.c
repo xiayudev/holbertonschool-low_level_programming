bb636193f6386f7c7efe2ccd305472326ebc1848
xiayudev <jctuesta7@gmail.com>
2023-02-27 20:50:29 -0800

    feat: add file 2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen - function
+ * @s: The string to be evaluated
+ * Swap values of two integers
+ *
+ * Return: Length of a string
+ *
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+	while (*s != '\0')
+	{
+		s++;
+		length++;
+	}
+	return (length);
+}
