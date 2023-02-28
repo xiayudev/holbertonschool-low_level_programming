8c7727662edc9c67f05dbbcef1c07d9c0091a6e3
xiayudev <jctuesta7@gmail.com>
2023-02-27 21:16:03 -0800

    feat: add file 4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - function
+ * @s: The string to be evaluated
+ * Prints a string, in reverse
+ *
+ * Return: Void
+ *
+ */
+void print_rev(char *s)
+{
+	char *init;
+
+	init = s;
+
+	while (*s != '\0')
+	{
+		s++;
+	}
+	while (s != init - 1)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar(10);
+}
