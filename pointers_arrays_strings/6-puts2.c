edf598064b055c1013656b009989a445252b8d2b
xiayudev <jctuesta7@gmail.com>
2023-02-28 06:37:32 -0800

    fix: add conditional within loop

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -10,10 +10,15 @@
  */
 void puts2(char *str)
 {
+	int i;
+
+	i = 0;
 	while (*str != '\0')
 	{
-		_putchar(*str);
-		str += 2;
+		if (i % 2 == 0)
+			_putchar(*str);
+		str++;
+		i++;
 	}
 	_putchar(10);
 }
