da8de861695fba9950fd1edf4543c426c9a314cb
xiayudev <jctuesta7@gmail.com>
2023-02-28 20:57:04 -0800

    feat: add file 0-strcat.c

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * strcat - function
+ * @dest: The character to be evaluated
+ * @src: The character to be evaluated
+ * Concatenates two strings
+ *
+ * Return: Pointer to the resulting dest
+ *
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	char *temp;
+
+	temp = dest;
+
+	while (*dest != '\0')
+		dest++;
+
+	i = 0;
+	while (*src != '\0')
+	{
+		*(dest + i) = *src;
+		i++;
+		src++;
+	}
+
+	/*Add nul character at the end*/
+	*(dest + i) = *src;
+	return (temp);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -25,4 +25,6 @@ char *_strcpy(char *dest, char *src);
 
 int _atoi(char *s);
 
+char *_strcat(char *dest, char *src);
+
 #endif
