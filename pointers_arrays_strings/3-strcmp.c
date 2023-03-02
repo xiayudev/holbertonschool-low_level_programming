4ada626182f8ad65c8c463fd600512c633df8eb4
xiayudev <jctuesta7@gmail.com>
2023-03-01 19:00:23 -0800

    feat: add file 3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -11,9 +11,30 @@
  *
  */
 
-int compare(char *s1, char *s2)
+int compare(char *s1, char *s2, int len)
 {
 	int i, result;
+
+	if (len < 0)
+	{
+		i = 0;
+		while (*(s2 + i) != '\0')
+		{
+			if (*(s1 + i) == *(s2 + i))
+			{
+				if (*(s2 + 1) == '\0')
+					return (0);
+				i++;
+				continue;
+			}
+			else
+			{
+				result = *(s1 + i) - *(s2 - i);
+				return (result);
+			}
+			i++;
+		}
+	}
 
 	i = 0;
 	while (*(s1 + i) != '\0')
@@ -47,7 +68,7 @@ int compare(char *s1, char *s2)
  */
 int _strcmp(char *s1, char *s2)
 {
-	int result;
+	int result, len;
 	short len_s1, len_s2;
 
 	len_s1 = 0;
@@ -58,19 +79,20 @@ int _strcmp(char *s1, char *s2)
 	while (*(s2 + len_s2) != '\0')
 		len_s2++;
 
+	len = len_s1 - len_s2;
 	if (len_s1 == len_s2)
 	{
-		result = compare(s1, s2);
+		result = compare(s1, s2, len);
 		return (result);
 	}
 	else if (len_s1 > len_s2)
 	{
-		result = compare(s1, s2);
+		result = compare(s1, s2, len);
 		return (result);
 	}
 	else
 	{
-		result = compare(s1, s2);
+		result = compare(s1, s2, len);
 		return (result);
 	}
 	return (0);
