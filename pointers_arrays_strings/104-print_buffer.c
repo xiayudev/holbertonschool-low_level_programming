e73d5134bcb9d52d81bfceba49264ea9d506482c
xiayudev <jctuesta7@gmail.com>
2023-03-05 19:58:36 -0800

    fix: add condition to print

--- pointers_arrays_strings/104-print_buffer.c
+++ pointers_arrays_strings/104-print_buffer.c
@@ -4,28 +4,45 @@
 /**
  * print - function
  * @b: The string to be encoded
- * @i: The number of bytes to be printed
+ * @i: The iterator
+ * @c: The character to print
+ * @size: Number of bytes to be printed
  * Print characters
  *
  * Return: Void
  *
  */
-void print(char *b, int i)
+void print(char *b, int i, char c, int size)
 {
-	if (*(b + i + 6) == '\0')
+	int j;
+
+	if (i % 10 == 0)
 	{
-		printf("%08x: %02x%02x %02x%02x %02x%02x	   %c",
-				i, *(b + i), *(b + i + 1), *(b + i + 2),
-				*(b + i + 3), *(b + i + 4), *(b + i + 5),
-				*(b + i));
+		printf("%08x: ", i);
+
+		j = i;
+		while (j < i + 10)
+		{
+			if (j >= size)
+			{
+				if (j % 2 == 0)
+					printf("  ");
+				else
+					printf("   ");
+			}
+			else
+			{
+				if (j % 2 == 0)
+					printf("%02x", *(b + j));
+				else
+					printf("%02x ", *(b + j));
+			}
+			j++;
+		}
+		printf(" %c", c);
 	}
 	else
-		printf("%08x: %02x%02x %02x%02x %02x%02x %02x%02x %02x%02x %c",
-				i, *(b + i), *(b + i + 1), *(b + i + 2),
-				*(b + i + 3), *(b + i + 4), *(b + i + 5),
-				*(b + i + 6), *(b + i + 7), *(b + i + 8),
-				*(b + i + 9), *(b + i));
-
+		printf("%c", c);
 }
 
 
@@ -45,30 +62,15 @@ void print_buffer(char *b, int size)
 	i = 0;
 	while (i < size)
 	{
-		if (i % 10 == 0)
+		if (i % 10 == 0 && i != 0)
 			putchar(10);
 		if (*(b + i) < 33 && *(b + i) != ' ')
 		{
-			if (i % 10 == 0)
-			{
-			printf(
-			"%08x: %02x%02x %02x%02x %02x%02x %02x%02x %02x%02x %c",
-				i, *(b + i), *(b + i + 1), *(b + i + 2),
-				*(b + i + 3), *(b + i + 4), *(b + i + 5),
-				*(b + i + 6), *(b + i + 7), *(b + i + 8),
-				*(b + i + 9), '.');
-			}
-			else
-				printf("%c", '.');
+			print(b, i, '.', size);
 		}
 		else
 		{
-			if (i % 10 == 0)
-			{
-				print(b, i);
-			}
-			else
-				printf("%c", *(b + i));
+			print(b, i, *(b + i), size);
 		}
 		i++;
 	}
