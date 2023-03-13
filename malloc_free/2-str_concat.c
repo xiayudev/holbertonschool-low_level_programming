2428e246c7644b2f6e431f717114a410517ceedf
xiayudev <jctuesta7@gmail.com>
2023-03-13 15:53:08 -0700

    fix: condition else if

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -18,7 +18,7 @@ char *str_concat(char *s1, char *s2)
 	/* Cheking if s1 or/and s2 is NULL */
 	if (s1 == NULL)
 		s1 = "";
-	else if (s2 == NULL)
+	if (s2 == NULL)
 		s2 = "";
 
 	/* Length of s1 and s2 */
