d494feb58bd32c1cfef4644e858d845f7db45589
xiayudev <jctuesta7@gmail.com>
2023-03-13 15:15:49 -0700

    fix: length of given string

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -12,9 +12,14 @@
 char *_strdup(char *str)
 {
 	int i;
-	char *new_str = malloc(sizeof(char) * sizeof(str));
+	char *new_str;
 
-	if (new_str == NULL)
+	i = 0;
+	while (*(str + i))
+		i++;
+	new_str = malloc(sizeof(char) * i);
+
+	if (new_str == NULL || str == NULL)
 		return (NULL);
 
 	i = 0;
@@ -23,6 +28,6 @@ char *_strdup(char *str)
 		*(new_str + i) = *(str + i);
 		i++;
 	}
-	*(new_str + i) = '\0';
+	*(new_str + i) = *(str + i);
 	return (new_str);
 }
