ae7666853946c49e077e946e0115c245068d5c80
xiayudev <jctuesta7@gmail.com>
2023-03-14 20:07:06 -0700

    fix: size of the array

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -17,7 +17,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	arr = malloc(sizeof(int) * (max - min) + 1);
+	arr = malloc(sizeof(int) * ((max - min) + 1));
 	if (arr == NULL)
 		return (NULL);
 	i = 0;
