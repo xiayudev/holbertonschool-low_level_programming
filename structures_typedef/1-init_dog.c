31f0491bcf4cb81ca7e0f19d8217ec796aabd3f2
xiayudev <jctuesta7@gmail.com>
2023-03-20 13:51:39 -0700

    fix: add a condition if NULL is passed in pointer to struct

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - function
@@ -14,7 +15,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
