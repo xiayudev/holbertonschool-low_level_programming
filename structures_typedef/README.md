7e570dc14cfb51c006a1233d0f22183e023bef92
xiayudev <jctuesta7@gmail.com>
2023-03-20 12:57:31 -0700

    feat: add README dog.h

---
+++ structures_typedef/README.md
@@ -0,0 +1,5 @@
+# Structures, typedef
+
+In this project we will cover:
+
+- Solving problems with malloc, free, exit, structures and typedef.
---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure declared
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Describe a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
