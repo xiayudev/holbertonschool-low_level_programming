0f3686127501dabad7d5f6080f17b01ec9316395
xiayudev <jctuesta7@gmail.com>
2023-03-20 14:27:17 -0700

    fix: if NULL is an element print nil

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -15,8 +15,14 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		printf("Name: %s\n", d->name);
-		printf("Age: %f\n", d->age);
-		printf("Owner: %s\n", d->owner);
+		(d->name)
+			? printf("Name: %s\n", d->name)
+			: printf("Name: %p\n", d->name);
+		(d->age)
+			? printf("Age: %f\n", d->age)
+			: printf("Age: %f\n", d->age);
+		(d->owner)
+			? printf("Owner: %s\n", d->owner)
+			: printf("Owner: %p\n", d->owner);
 	}
 }
