a7af1b79ebf3f32858413f4cacc964f9c6f06134
xiayudev <jctuesta7@gmail.com>
2023-03-20 20:28:17 -0700

    feat: free dog structure and delete unnecessary functions

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_dog - function
+ * @d: The struct dog
+ *
+ * Frees dogs
+ *
+ * Return: Void
+ *
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
