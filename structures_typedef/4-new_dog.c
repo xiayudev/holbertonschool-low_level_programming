6e15f778e42b240e7079e0d2d94369ac5d5b851f
xiayudev <jctuesta7@gmail.com>
2023-03-20 18:52:01 -0700

    fix: copied string

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,15 +1,35 @@
 #include "dog.h"
 #include <stdlib.h>
 
+/**
+ * _strlen - function
+ * @s: The string to be evaluated
+ *
+ * Length of the string
+ *
+ * Return: The length of the string
+ *
+ */
+
 int _strlen(char *s)
 {
-        int length;
+	int length;
 
-        length = 0;
-        while (*(s + length))
-                length++;
-        return (length);
+	length = 0;
+	while (*(s + length))
+		length++;
+	return (length);
 }
+
+/**
+ * _strdup - function
+ * @str: The string to be copied
+ *
+ * Duplicate a string
+ *
+ * Return: The pointer to the duplicated string
+ *
+ */
 
 char *_strdup(char *str)
 {
@@ -35,6 +55,32 @@ char *_strdup(char *str)
 	*(new_str + i) = *(str + i);
 	return (new_str);
 }
+
+/**
+ * _strcpy - function
+ * @dest: New string copied
+ * @src: The string to be copied
+ * Copy an string to another variable
+ *
+ * Return: Pointer to dest
+ *
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	/*How to take nul character*/
+	while (*src != '\0')
+	{
+		*(dest + i) = *src;
+		src++;
+		i++;
+	}
+	*(dest + i) = *src;
+	return (dest);
+}
+
 /**
  * new_dog - function
  * @name: The name of the dog
@@ -57,26 +103,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	c_name = _strdup(name);
 	c_owner = _strdup(owner);
-	my_dog->name = malloc(sizeof _strlen(name + 1));
-	if (!my_dog->name)
-	{
-		free(my_dog->owner);
-		free(my_dog->name);
-		free(my_dog);
-		return (NULL);
-	}
+
 	my_dog->name = c_name;
-
 	my_dog->age = age;
-
-	my_dog->owner = malloc(sizeof _strlen(owner + 1));
-	if (!my_dog->owner)
-	{
-		free(my_dog->owner);
-		free(my_dog->name);
-		free(my_dog);
-		return (NULL);
-	}
 	my_dog->owner = c_owner;
 	return (my_dog);
 }
