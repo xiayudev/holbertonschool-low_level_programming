f7167495a3e0cbeb340dd40f73cc1b459cf096cc
xiayudev <jctuesta7@gmail.com>
2023-04-10 20:59:57 -0700

    fix: make buffer dynamic

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -2,6 +2,7 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 
 /**
@@ -17,26 +18,36 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t fd, bytes, wr;
-	int i;
-	char buff[1024];
+	size_t i;
+	char *buff;
 
 	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY, S_IRUSR);
 	if (fd == -1)
 		return (0);
+
+	buff = malloc(letters + 1);
+	if (!buff)
+		return (0);
+
+	/* Initialize the buffer with nul-character*/
 	i = 0;
-	while (i < 1024)
+	while (i < letters)
 	{
-		buff[i] = '\0';
+		*(buff + i) = '\0';
 		i++;
 	}
+	*(buff + i) = '\0';
+
 	bytes = read(fd, buff, letters);
 	if (bytes == -1)
 		return (0);
+
 	wr = write(1, buff, bytes);
 	if (wr == -1 || wr != bytes)
 		return (0);
 	close(fd);
+	free(buff);
 	return (wr);
 }
