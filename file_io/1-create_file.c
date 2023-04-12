964f3e170acd1b6630aa12bd3da22285f844b68b
xiayudev <jctuesta7@gmail.com>
2023-04-11 19:50:04 -0700

    feat: create a file if it does not exit and insert text on it

---
+++ file_io/1-create_file.c
@@ -0,0 +1,59 @@
+#include "main.h"
+#include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * _strlen - function
+ * @s: The string to be evaluated
+ * Swap values of two integers
+ *
+ * Return: Length of a string
+ *
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+	while (*(s + length))
+	{
+		length++;
+	}
+	return (length);
+}
+
+/**
+ * create_file - function to create a file
+ * @filename: The name of the file to be created
+ * @text_content: The string to be added
+ *
+ * Create a file and add content to it
+ *
+ * Return: 1 success, -1 otherwise
+ *
+ */
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t fd, wr;
+	int len;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	len = _strlen(text_content);
+	wr = (!len) ? write(fd, "", len) : write(fd, text_content, len);
+	if (wr == -1)
+	{
+		close(fd);
+		return (0);
+	}
+	close(fd);
+	return (1);
+}
