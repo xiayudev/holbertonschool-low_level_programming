34fe7f83062ae9884acb40b04e10ca651531104a
xiayudev <jctuesta7@gmail.com>
2023-04-15 07:17:11 -0700

    feat: delete hash table

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function to delete
+ * @ht: The hash table
+ *
+ * Delete a hash table
+ *
+ * Return: Void
+ *
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *temp, *delete;
+
+	i = 0;
+	while (i < ht->size)
+	{
+		temp = ht->array[i];
+		delete = temp;
+		while (temp)
+		{
+			free(temp->key);
+			free(temp->value);
+			temp = temp->next;
+			free(delete);
+			delete = temp;
+		}
+		i++;
+	}
+	free(ht->array);
+	free(ht);
+}
