b7c7fd11c2d0f21fa95cf17666865ebf0c23f991
xiayudev <jctuesta7@gmail.com>
2023-04-14 18:10:07 -0700

    fix: add condition if hash table is null

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -104,6 +104,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *node;
 	unsigned long int index;
 
+	if (!ht)
+		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	node = ht->array[index];
 	while (node)
